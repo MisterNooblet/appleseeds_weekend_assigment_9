3b30843cb92dae7231d4e24bcaf4c438ba6fe32c
misternooblet <dragonroadsupport@gmail.com>
2023-02-23 16:42:23 +0200

    header done

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <main>
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to={'/'}>HOME</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/game'}>GAME</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+            </main>
+            <Outlet />
+            <Footer />
+        </>
     )
 }
 
