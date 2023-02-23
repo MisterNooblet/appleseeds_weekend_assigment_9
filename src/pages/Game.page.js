c3de5b54678c0dc9c0eb36eb028dcd656b323c25
misternooblet <dragonroadsupport@gmail.com>
2023-02-23 17:39:08 +0200

    Game layout

--- src/pages/Game.page.js
+++ src/pages/Game.page.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import data from '../data/data'
 
 const Game = () => {
     return (
-        <div>Game</div>
+        <section>
+            <div className='game_top'>
+                <div>Score 1</div>
+                <div>Score 2</div>
+            </div>
+            <div className='game_main'>Image</div>
+            <div className='game_bottom'>
+                <button>X</button>
+                <button>V</button>
+            </div>
+        </section>
     )
 }
 
