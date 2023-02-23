0812da3a912aeb44866c155c75f4db1afb6dc7e1
misternooblet <dragonroadsupport@gmail.com>
2023-02-23 17:11:21 +0200

    homepage done

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section>
+            <h1>Welcome to my guessing game</h1>
+            <h3>You will be presented with Images of tools and a corresponding name</h3>
+            <h3>You will than have to guess if the name fits the tool on the Image or not</h3>
+            <h3>On top of the image you will find two counters the left one for the wrong guesses and the right one for the right ones</h3>
+            <Link to={'/game'}><button>Continue to game...</button></Link>
+        </section>
     )
 }
 
