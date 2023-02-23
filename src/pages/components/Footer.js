0e1d2d2afc40901e3e6d0f73fc335595db0338da
misternooblet <dragonroadsupport@gmail.com>
2023-02-23 16:45:45 +0200

    Footer Done

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import { FaGithubAlt } from 'react-icons/fa';
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <ul>
+                <li>
+                    <h3>Github Repository: <a href='https://github.com/MisterNooblet/appleseeds_weekend_assigment_9'><FaGithubAlt /></a></h3>
+                </li>
+            </ul>
+        </div>
     )
 }
 
