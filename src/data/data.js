4e7028e7395f88f928906cf88ca7fe53d5240808
misternooblet <dragonroadsupport@gmail.com>
2023-02-23 16:30:31 +0200

    data done

---
+++ src/data/data.js
@@ -0,0 +1,104 @@
+const data = [
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkUUlUU0G1XO5l6tgTPUKz2dRq-d8Ip4Pk3g&usqp=CAU',
+        name: 'Aviation Snips',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkUUlUU0G1XO5l6tgTPUKz2dRq-d8Ip4Pk3g&usqp=CAU',
+        name: 'Arc Welder',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL0ePVnTrylW7n436R45FxnteskQf7OlkBkQ&usqp=CAU',
+        name: 'Pipe Wrench',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL0ePVnTrylW7n436R45FxnteskQf7OlkBkQ&usqp=CAU',
+        name: 'Adjustable Screwdriver',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2GI1xGX4hBJFWcJZWcY_gQhn3fCCr9V0nsQ&usqp=CAU',
+        name: 'Ratcheting Wrench',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2GI1xGX4hBJFWcJZWcY_gQhn3fCCr9V0nsQ&usqp=CAU',
+        name: 'Adjustable Bolt Screwer',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0zZwTRhd3Q5z0KLx-YGHvjbruRli8GZEcEw&usqp=CAU',
+        name: 'Portable Hair Styler',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0zZwTRhd3Q5z0KLx-YGHvjbruRli8GZEcEw&usqp=CAU',
+        name: 'Electric Air Blower',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2LkN9FAvZv4juhVaV2wKaYdqTUj89B6KVyQ&usqp=CAU',
+        name: 'Hammer',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2LkN9FAvZv4juhVaV2wKaYdqTUj89B6KVyQ&usqp=CAU',
+        name: 'Hitting Device',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNIg5CIzsLlV7RNFcYH6HYMy9IBj8waUs9TA&usqp=CAU',
+        name: 'Chisel',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNIg5CIzsLlV7RNFcYH6HYMy9IBj8waUs9TA&usqp=CAU',
+        name: 'Stisel',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRroCNyRcUkeT_19VRZ6EmvpL6uQ67-GSrGOA&usqp=CAU',
+        name: 'Angle Grinder',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRroCNyRcUkeT_19VRZ6EmvpL6uQ67-GSrGOA&usqp=CAU',
+        name: 'Spinny-Spinny Cut-Cut',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSEt1-x8SEtuX3oAvpHc6xjH2yf8a1nDoXDA&usqp=CAU',
+        name: 'Arc Welder',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSEt1-x8SEtuX3oAvpHc6xjH2yf8a1nDoXDA&usqp=CAU',
+        name: 'Sewing Machine',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-UxA4Aq_dnhFlbq6AuONKu54lArfILi7T6w&usqp=CAU',
+        name: 'Electrode',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-UxA4Aq_dnhFlbq6AuONKu54lArfILi7T6w&usqp=CAU',
+        name: 'Metal Stick',
+        answer: false,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTULmfK-_-3X4gNux5bcrbuZLi4u3JgF0xR5g&usqp=CAU',
+        name: 'Flat File',
+        answer: true,
+    },
+    {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTULmfK-_-3X4gNux5bcrbuZLi4u3JgF0xR5g&usqp=CAU',
+        name: 'Nail File',
+        answer: false,
+    },
+]
+
+export default data;
