bba8fd988ea90d0d4f6fa4143d2de39cae4c8a91
misternooblet <dragonroadsupport@gmail.com>
2023-02-23 16:50:39 +0200

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./pages/components/Header";
+import Game from "./pages/Game.page";
+import Home from "./pages/Home.page";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/game', element: <Game /> }
+    ]
+  }
+])
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
